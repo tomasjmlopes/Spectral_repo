2894cf07acc6faee7891ca2f7c113db7339130bb
official-zemeister <tomas.lopes02@gmail.com>
2024-08-09 17:34:59 +0100

    Update

--- Features/hdr_funcs.py
+++ Features/hdr_funcs.py
@@ -5,6 +5,9 @@ from skimage.morphology import disk
 from skimage.filters import rank
 from scipy.ndimage import convolve
 
+from scipy.fftpack import fft2, ifft2
+from skimage.transform import resize
+
 from tqdm import tqdm
 
 KERNEL = (1.0/256) * np.array([
@@ -15,7 +18,7 @@ KERNEL = (1.0/256) * np.array([
     [1, 4, 6, 4, 1]
 ])
 
-def normalize_spectral_images(data, use_gaussian=False, sigma=0.5):
+def normalize_spectral_images(data, use_gaussian=False, sigma=0.5, batch_norm = True):
     """
     Normalizes spectral images using a min-max scaling scheme with optional Gaussian filtering.
 
@@ -33,27 +36,28 @@ def normalize_spectral_images(data, use_gaussian=False, sigma=0.5):
     else:
         filtered_data = data
 
-    data_min = filtered_data.min()
-    data_max = filtered_data.max()
-    
-    for wv in tqdm(range(data.shape[0]), desc="Normalizing images"):
-        # if use_gaussian:
-        #     filtered_data = gaussian_filter(data[wv], sigma=sigma)
-        # else:
-        #     filtered_data = data[wv]
+    if batch_norm:
+        data_min = filtered_data.min()
+        data_max = filtered_data.max()
         
-        # data_min = filtered_data.min()
-        # data_max = filtered_data.max()
-        
-        if data_max > data_min:
-            # normalized_data[wv] = (filtered_data - data_min) / (data_max - data_min)
-            normalized_data[wv] = (filtered_data[wv] - data_min) / (data_max - data_min)
-        else:
-            normalized_data[wv] = 0
+        for wv in tqdm(range(data.shape[0]), desc="Normalizing images"):
+            if data_max > data_min:
+                normalized_data[wv] = (filtered_data[wv] - data_min) / (data_max - data_min)
+            else:
+                normalized_data[wv] = 0
+    else: 
+        for wv in tqdm(range(data.shape[0]), desc="Normalizing images"):  
+            data_min = filtered_data[wv].min()
+            data_max = filtered_data[wv].max()
+            
+            if data_max > data_min:
+                normalized_data[wv] = (filtered_data[wv] - data_min) / (data_max - data_min)
+            else:
+                normalized_data[wv] = 0
 
     return normalized_data
 
-def assemble_hdr(labels, dataset, use_gaussian=False, sigma=0.5):
+def assemble_hdr(labels, dataset, use_gaussian=False, sigma=0.5, batch_norm=True):
     """
     Assembles a high dynamic range (HDR) dataset from labeled data, applying normalization to each unique label group.
 
@@ -67,7 +71,7 @@ def assemble_hdr(labels, dataset, use_gaussian=False, sigma=0.5):
         np.ndarray: An array of HDR processed data groups, each corresponding to a unique label.
     """
     unique_labels = np.unique(labels)
-    hdr_dataset = [normalize_spectral_images(np.array(dataset[labels == label]), use_gaussian=use_gaussian, sigma=sigma)
+    hdr_dataset = [normalize_spectral_images(np.array(dataset[labels == label]), use_gaussian=use_gaussian, sigma=sigma, batch_norm=batch_norm)
                    for label in unique_labels]
     
     return np.array(hdr_dataset, dtype=object)
@@ -76,9 +80,20 @@ def gauss(x, sigma, val):
     """ Gaussian function for calculating exposedness. """
     return np.exp(-((x - val) ** 2) / (2 * sigma ** 2))
 
-def calculate_weights(hdr_dataset, sigma=0.2, ideal_val=0.5):
+def saliency_map(img):
     """
-    Calculate weights for HDR assembly based on exposedness and sharpness.
+    Calculate a simple saliency map based on local contrast.
+    """
+
+    local_mean = gaussian_filter(img, sigma = 30)
+    local_contrast = np.abs(img - local_mean)
+    saliency = (local_contrast - local_contrast.min()) / (local_contrast.max() - local_contrast.min() + 1e-8)
+    
+    return saliency
+
+def calculate_weights(hdr_dataset, sigma=0.2, ideal_val=0.5, beta=0, weight_exp = 1, weight_sharp = 1, weight_saliency = 1):
+    """
+    Calculate weights for HDR assembly based on various metrics.
     This function now handles hdr_dataset that may include object arrays.
     """
     weights_norm = []
@@ -86,10 +101,17 @@ def calculate_weights(hdr_dataset, sigma=0.2, ideal_val=0.5):
         if isinstance(data, np.ndarray) and data.dtype == object:
             data = np.stack(data).astype(float)
 
-        w_exposedness = gauss(data, sigma, ideal_val)
+        # Ideal value for vision is 0.5. However, by introducing the beta
+        # parameter, we can balance this ideal value with the average intensity
+        # values of the image.
+        avg_int = np.mean(data, axis = 0)
+        mu = (1 - beta) * ideal_val + beta * avg_int
 
-        metric_sharpness = np.array([abs(laplace(data[i])) for i in tqdm(range(data.shape[0]), desc="Calculating sharpness")])
-        weights = w_exposedness * metric_sharpness
+        w_exposedness = gauss(data, sigma, mu)
+        metric_sharpness = np.array([abs(laplace(data[i])) for i in range(data.shape[0])])
+        metric_saliency = np.array([saliency_map(data[i]) for i in range(data.shape[0])])
+
+        weights = w_exposedness**weight_exp * metric_sharpness**weight_sharp * metric_saliency**weight_saliency
 
         w_sum = np.sum(weights, axis = 0, keepdims = True)
         weights_each = weights / w_sum
